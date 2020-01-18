03573a4d2dac1b211ae63810cf7841965f828c48
erasels <emanuelbedburdick@yahoo.de>
2020-01-18 18:22:32 +0100

    Fixed first monster intent not being set

--- src/main/java/wanderingMiniBosses/actions/CustomSpawnMonsterAction.java
+++ src/main/java/wanderingMiniBosses/actions/CustomSpawnMonsterAction.java
@@ -1,6 +1,9 @@
 package wanderingMiniBosses.actions;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.RollMoveAction;
 import com.megacrit.cardcrawl.actions.common.SpawnMonsterAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 public class CustomSpawnMonsterAction extends SpawnMonsterAction {
@@ -16,6 +19,14 @@ public class CustomSpawnMonsterAction extends SpawnMonsterAction {
         super.update();
         if(isDone) {
             mon.usePreBattleAction();
+            AbstractDungeon.actionManager.addToTop(new AbstractGameAction() {
+                @Override
+                public void update() {
+                    mon.createIntent();
+                    isDone = true;
+                }
+            });
+            AbstractDungeon.actionManager.addToTop(new RollMoveAction(mon));
         }
     }
 }
