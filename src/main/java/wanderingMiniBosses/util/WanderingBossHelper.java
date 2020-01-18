386652a943218a233c41b0226ae2b20682b72b96
erasels <emanuelbedburdick@yahoo.de>
2020-01-18 13:44:23 +0100

    Added logic to the getMonster methods

--- src/main/java/wanderingMiniBosses/util/WanderingBossHelper.java
+++ src/main/java/wanderingMiniBosses/util/WanderingBossHelper.java
@@ -1,9 +1,14 @@
 package wanderingMiniBosses.util;
 
+import basemod.BaseMod;
+import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.Map;
 
 public class WanderingBossHelper {
     private static AbstractMonster wanderingBoss;
+    public static Map<String, BaseMod.GetMonster> monsterMap;
 
     public static AbstractMonster getMonster() {
         return wanderingBoss;
@@ -15,11 +20,12 @@ public class WanderingBossHelper {
 
     //TODO: Make actual logic
     public static AbstractMonster getMonsterFromID(String id) {
-        return null;
+        return monsterMap.get(id).get();
     }
 
     //TODO: Make actual logic
     public static AbstractMonster getRandomMonster() {
-        return null;
+        //Using a non-seeded random should be alright since this should only be run once per dungeon creation and the result is saved.
+        return (AbstractMonster) monsterMap.values().toArray()[MathUtils.random(monsterMap.size()-1)];
     }
 }
