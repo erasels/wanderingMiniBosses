29a7b1c94f13b88c792feaabf258bdfe732f82cb
erasels <emanuelbedburdick@yahoo.de>
2020-01-18 10:36:35 +0100

    Update BottledPlaceholderRelic.java

--- src/main/java/wanderingMiniBosses/relics/BottledPlaceholderRelic.java
+++ src/main/java/wanderingMiniBosses/relics/BottledPlaceholderRelic.java
@@ -16,7 +16,6 @@ import com.megacrit.cardcrawl.helpers.PowerTip;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import wanderingMiniBosses.WanderingminibossesMod;
-import wanderingMiniBosses.patches.relics.BottledPlaceholderField;
 import wanderingMiniBosses.util.TextureLoader;
 
 import java.util.Iterator;
