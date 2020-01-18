178b7fa72c8271160b79d21078a12da477ad7265
erasels <emanuelbedburdick@yahoo.de>
2020-01-18 12:35:38 +0100

    Create WanderingBossHelper.java

---
+++ src/main/java/wanderingMiniBosses/util/WanderingBossHelper.java
@@ -0,0 +1,11 @@
+package wanderingMiniBosses.util;
+
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class WanderingBossHelper {
+    public static AbstractMonster wanderingBoss;
+
+    public static AbstractMonster getMonsterFromID(String id) {
+        return null;
+    }
+}
