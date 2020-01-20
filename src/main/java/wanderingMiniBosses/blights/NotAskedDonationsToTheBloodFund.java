c9fbb5506ca6ab4d560383c2ada5b00ccf2f03c8
Clauvin <clauvin_almeida@hotmail.com>
2020-01-20 02:52:47 -0300

    Added NotAskedDonationsToTheBloodFund blight

---
+++ src/main/java/wanderingMiniBosses/blights/NotAskedDonationsToTheBloodFund.java
@@ -0,0 +1,41 @@
+package wanderingMiniBosses.blights;
+
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.blights.AbstractBlight;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.AbstractCard.CardType;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.BlightStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class NotAskedDonationsToTheBloodFund extends AbstractBlight {
+
+	public static final String ID = "NotAskedDonationsToTheBloodFund";
+	private static final BlightStrings blightStrings = 
+			CardCrawlGame.languagePack.getBlightString("NotAskedDonationsToTheBloodFund");
+	public static final String NAME = blightStrings.NAME;
+	public static final String[] DESC = blightStrings.DESCRIPTION;
+
+	public static final int AMOUNT_OF_DAMAGE_TAKEN = 3;
+	
+	public NotAskedDonationsToTheBloodFund() {
+		super(ID, NAME, DESC[0] + AMOUNT_OF_DAMAGE_TAKEN + DESC[1], "void.png", false);
+	}
+	
+    @Override
+    public void onPlayCard(AbstractCard card, AbstractMonster m) {
+    	if (card.type == CardType.POWER) {
+    		AbstractDungeon.actionManager.
+    			addToBottom(
+    				new DamageAction(
+    					AbstractDungeon.player,
+    					new DamageInfo(AbstractDungeon.player, AMOUNT_OF_DAMAGE_TAKEN)));
+    		
+    		this.flash();
+    	}
+    	super.onPlayCard(card, m);
+    }
+    
+}
