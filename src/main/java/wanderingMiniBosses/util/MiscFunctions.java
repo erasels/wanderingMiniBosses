561562d66feb9e208bbcc03841882079ed393503
Clauvin <clauvin_almeida@hotmail.com>
2020-01-20 02:51:41 -0300

    Added some misc functions to MiscFunctions

--- src/main/java/wanderingMiniBosses/util/MiscFunctions.java
+++ src/main/java/wanderingMiniBosses/util/MiscFunctions.java
@@ -1,5 +1,30 @@
 package wanderingMiniBosses.util;
 
+import wanderingMiniBosses.WanderingminibossesMod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.megacrit.cardcrawl.random.Random;
+
 public class MiscFunctions {
 
+	// lets us log output
+	// for faster debugging
+	public static final Logger logger = LogManager.getLogger(WanderingminibossesMod.class.getName());
+
+	public static void fastLoggerLine(String message) {
+    	logger.info(message);
+    }
+    
+    public static void fastLoggerLine(Boolean message) {
+    	String converted_message = message.toString();
+    	fastLoggerLine(converted_message);
+    }
+    
+    public static int headsOrTails(Random random) {
+		return random.random(1);
+	}
+	
+	
 }
