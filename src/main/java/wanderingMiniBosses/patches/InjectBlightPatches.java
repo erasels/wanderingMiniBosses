aa28bf82d7eef90dc98100e8b2f99842edb0f91a
erasels <emanuelbedburdick@yahoo.de>
2020-01-20 11:09:53 +0100

    Made Blights save and load correctly

--- src/main/java/wanderingMiniBosses/patches/InjectBlightPatches.java
+++ src/main/java/wanderingMiniBosses/patches/InjectBlightPatches.java
@@ -6,13 +6,20 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.blights.AbstractBlight;
 import com.megacrit.cardcrawl.helpers.BlightHelper;
+import wanderingMiniBosses.blights.FullOfOpenings;
+import wanderingMiniBosses.blights.NotAskedDonationsToTheBloodFund;
+import wanderingMiniBosses.blights.TooCautious;
 
 @SpirePatch(clz = BlightHelper.class, method = "getBlight")
 public class InjectBlightPatches {
     @SpirePrefixPatch
     public static SpireReturn<AbstractBlight> returnBlight(String ID) {
-        if (ID.equals()) {
-            return SpireReturn.Return(new );
+        if (ID.equals(FullOfOpenings.ID)) {
+            return SpireReturn.Return(new FullOfOpenings());
+        } else if (ID.equals(TooCautious.ID)) {
+            return SpireReturn.Return(new TooCautious());
+        } else if (ID.equals(NotAskedDonationsToTheBloodFund.ID)) {
+            return SpireReturn.Return(new NotAskedDonationsToTheBloodFund());
         }
 
         return SpireReturn.Continue();
