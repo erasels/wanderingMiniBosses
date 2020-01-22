4e96b47a5b678d7dc3cc002c19e91013ede3a179
Raz <razash@gmail.com>
2020-01-22 07:20:14 +0100

    Implemented gremlin knight moveset.

--- src/main/java/wanderingMiniBosses/monsters/gremlinknight/GremlinKnight.java
+++ src/main/java/wanderingMiniBosses/monsters/gremlinknight/GremlinKnight.java
@@ -4,16 +4,26 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.MathUtils;
 import com.esotericsoftware.spine.AnimationState;
 import com.evacipated.cardcrawl.modthespire.lib.SpireOverride;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.TalkAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.MonsterStrings;
 import com.megacrit.cardcrawl.monsters.EnemyMoveInfo;
+import com.megacrit.cardcrawl.monsters.city.Champ;
+import com.megacrit.cardcrawl.powers.*;
 import com.megacrit.cardcrawl.vfx.SpeechBubble;
 import wanderingMiniBosses.WanderingminibossesMod;
 import wanderingMiniBosses.actions.RammingEntranceAction;
 import wanderingMiniBosses.monsters.AbstractWanderingBoss;
+
+import java.util.ArrayList;
 
 public class GremlinKnight extends AbstractWanderingBoss {
     public static final String ID = WanderingminibossesMod.makeID("GremlinKnight");
@@ -22,14 +32,21 @@ public class GremlinKnight extends AbstractWanderingBoss {
     public static final String[] MOVES;
     public static final String[] DIALOG;
 
-    private static final byte SUMMONDAGGER = 0;
+    private static final byte STAB = 0;
+    private static final byte STABSTAB = 1;
+    private static final byte BONK = 2;
+    private static final byte TAUNT = 3;
 
     private final int entranceThreshold = 3;
 
     public GremlinKnight() {
-        super(NAME, ID, 400, 0.0F, 10.0F, 230.0F, 230.0F, null, 0, -30);
+        super(NAME, ID, 200, 0.0F, 10.0F, 230.0F, 230.0F, null, 0, -30);
+        this.isPlayer = true;
         this.drawX = -this.hb.width;
         this.flipHorizontal = true;
+
+        this.dialogX = this.hb.width / 2F;
+        this.dialogY = this.hb.height / 2F;
 
         this.loadAnimation(WanderingminibossesMod.makeMonsterPath("gremlinknight/skeleton.atlas"), WanderingminibossesMod.makeMonsterPath("gremlinknight/skeleton.json"), 1.0F);
         AnimationState.TrackEntry e = this.state.setAnimation(0, "idle", true);
@@ -38,26 +55,69 @@ public class GremlinKnight extends AbstractWanderingBoss {
 
     @Override
     protected void populateMoves() {
-        this.moves.put(SUMMONDAGGER, new EnemyMoveInfo(SUMMONDAGGER, Intent.UNKNOWN, -1, 0, false));
+        this.moves.put(STAB, new EnemyMoveInfo(STAB, Intent.ATTACK, 6 * AbstractDungeon.actNum, 0, false));
+        this.moves.put(STABSTAB, new EnemyMoveInfo(STABSTAB, Intent.ATTACK, 3, 1 + AbstractDungeon.actNum, true));
+        this.moves.put(BONK, new EnemyMoveInfo(BONK, Intent.ATTACK_DEBUFF, 3 + 2 * AbstractDungeon.actNum, 0, false));
+        this.moves.put(TAUNT, new EnemyMoveInfo(TAUNT, Intent.DEBUFF, -1, 0, false));
     }
 
     @Override
     public void usePreBattleAction() {
         super.usePreBattleAction();
         AbstractDungeon.effectList.add(new SpeechBubble(Settings.WIDTH * 0.01F, AbstractDungeon.floorY + 150F * Settings.scale, DIALOG[MathUtils.random(entranceThreshold - 1)], true));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(this, this, new AngryPower(this, 1)));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(this, this, new BarricadePower(this)));
+        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(this, this, 25 + 25 * AbstractDungeon.actNum));
         AbstractDungeon.actionManager.addToBottom(new RammingEntranceAction(AbstractDungeon.player, this));
     }
 
     @Override
     public void takeCustomTurn(DamageInfo info, int multiplier) {
         switch(this.nextMove) {
+            case STAB:
+                this.useFastAttackAnimation();
+                AbstractDungeon.actionManager.addToBottom(new DamageAction(AbstractDungeon.player, info, AbstractGameAction.AttackEffect.SMASH));
+                break;
+
+            case STABSTAB:
+                this.useFastAttackAnimation();
+                for(int i = 0; i < multiplier; i++) {
+                    AbstractGameAction.AttackEffect tmp = null;
+                    switch(MathUtils.random(2)) {
+                        case 0:
+                            tmp = AbstractGameAction.AttackEffect.SLASH_DIAGONAL;
+                            break;
+                        case 1:
+                            tmp = AbstractGameAction.AttackEffect.SLASH_HORIZONTAL;
+                            break;
+                        case 2:
+                            tmp = AbstractGameAction.AttackEffect.SLASH_VERTICAL;
+                            break;
+                    }
+                    AbstractDungeon.actionManager.addToBottom(new DamageAction(AbstractDungeon.player, info, tmp));
+                }
+                break;
+
+            case BONK:
+                this.useFastAttackAnimation();
+                AbstractDungeon.actionManager.addToBottom(new DamageAction(AbstractDungeon.player, info, AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, this, new FrailPower(this, 1, true), 1));
+                break;
+
+            case TAUNT:
+                AbstractDungeon.actionManager.addToBottom(new SFXAction("VO_CHAMP_2A", 2F, true));
+                AbstractDungeon.actionManager.addToBottom(new TalkAction(this, Champ.DIALOG[MathUtils.random(3)]));
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, this, new WeakPower(AbstractDungeon.player, 2, true), 2));
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, this, new VulnerablePower(AbstractDungeon.player, 2, true), 2));
+                break;
         }
     }
 
     @Override
     public void onEscape() {
         super.onEscape();
-        AbstractDungeon.effectList.add(new SpeechBubble(this.drawX + this.hb.width / 2F, this.drawY + this.hb.height / 2F, DIALOG[entranceThreshold + MathUtils.random(DIALOG.length - entranceThreshold - 1)], true));
+        this.flipHorizontal = !this.flipHorizontal;
+        AbstractDungeon.actionManager.addToBottom(new TalkAction(this, DIALOG[entranceThreshold + MathUtils.random(DIALOG.length - entranceThreshold - 1)]));
     }
 
     static {
@@ -70,9 +130,8 @@ public class GremlinKnight extends AbstractWanderingBoss {
     @SpireOverride
     protected void updateEscapeAnimation() {
         if (this.escapeTimer != 0.0F) {
-            this.flipHorizontal = false;
             this.escapeTimer -= Gdx.graphics.getDeltaTime();
-            this.drawX -= Gdx.graphics.getDeltaTime() * 400.0F * Settings.scale;
+            this.drawX += Gdx.graphics.getDeltaTime() * 400.0F * Settings.scale * (flipHorizontal ? 1 : -1);
         }
 
         if (this.escapeTimer < 0.0F) {
@@ -81,6 +140,5 @@ public class GremlinKnight extends AbstractWanderingBoss {
                 AbstractDungeon.getCurrRoom().endBattle();
             }
         }
-
     }
 }
