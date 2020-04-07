bd09c2207678f99ae56f754f9a20e96a0b25c334
Cláuvin <Clauvin@users.noreply.github.com>
2020-04-06 22:33:40 -0300

    Trying to fix why FullOfOpenings store its values through new runs, part 2

--- src/main/java/wanderingMiniBosses/blights/FullOfOpenings.java
+++ src/main/java/wanderingMiniBosses/blights/FullOfOpenings.java
@@ -28,8 +28,12 @@ public class FullOfOpenings extends AbstractBlight implements CustomSavable<Arra
 
 	public static final String FULL_OF_OPENINGS_SAVE_FIELD_KEY =
 			"wandering_mini_bosses_full_openings_data";
-	public static int AMOUNT_OF_GOLD_STOLEN = 5;
-	public static int AMOUNT_OF_GOLD_LEFT_TO_LOSE = 200;
+	public static final int INITIAL_AMOUNT_OF_GOLD_STOLEN = 5;
+	public static int AMOUNT_OF_GOLD_STOLEN =
+			INITIAL_AMOUNT_OF_GOLD_STOLEN;
+	public static final int INITIAL_AMOUNT_OF_GOLD_LEFT_TO_LOSE = 200;
+	public static int AMOUNT_OF_GOLD_LEFT_TO_LOSE =
+			INITIAL_AMOUNT_OF_GOLD_LEFT_TO_LOSE;
 
 	
 	public FullOfOpenings() {
@@ -88,15 +92,20 @@ public class FullOfOpenings extends AbstractBlight implements CustomSavable<Arra
 				AbstractDungeon.player.loseGold(AMOUNT_OF_GOLD_STOLEN);
 				AMOUNT_OF_GOLD_LEFT_TO_LOSE -= AMOUNT_OF_GOLD_STOLEN;
 				this.updateTips();
-				if (AMOUNT_OF_GOLD_LEFT_TO_LOSE > 0) this.flash();
+				if (AMOUNT_OF_GOLD_LEFT_TO_LOSE > 0){
+					this.flash();
+					CardCrawlGame.sound.playA("VO_LOOTER_1A", 0.2F);
+					CardCrawlGame.sound.playA("VO_LOOTER_1B", 0.5F);
+					CardCrawlGame.sound.playA("VO_LOOTER_1C", 0.0F);
+				}
 				counter = 0;
 			}
 		}
 
 		if (AMOUNT_OF_GOLD_LEFT_TO_LOSE <= 0) {
+
 			AbstractDungeon.actionManager.addToBottom(
 					new RemoveSpecificBlightAction(this));
-
 		}
 	}
 }
